c4747d7ff76c941055856c8db4618bfbeed9bae5
TeranRK <kevin.teranr@ug.edu.ec>
2025-06-10 17:57:04 -0500

    Descripción de los cambios

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 import Header from './components/header';
 import Footer from './components/footer';
-
+import Script from 'next/script';
 
 export const metadata = {
   title: 'SaludPet',
@@ -11,19 +11,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LTR1CZ4WQY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LTR1CZ4WQY');
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col min-h-screen">
-        
-        <Header>
-        {/*<!-- Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LTR1CZ4WQY"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-LTR1CZ4WQY');
-        </script>
-        </Header>
+        <Header />
         <main className="flex-grow p-4 bg-main">{children}</main>
         <Footer />
       </body>
