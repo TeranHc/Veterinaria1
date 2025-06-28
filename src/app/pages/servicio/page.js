001e020a9906199933665707b979a6291cfa4da9
Steven <stevenpluas.22@hotmail.com>
2025-06-28 09:32:07 -0500

    Pruebas rutas

--- src/app/pages/servicio/page.js
+++ src/app/pages/servicio/page.js
@@ -129,7 +129,7 @@ export default function Servicios() {
             <button 
               onClick={() => {
                 const routes = {
-                  consulta: '/servicio/generales',
+                  consulta: '/pages/servicio/generales',
                   laboratorio: '/servicios/laboratorio',
                   teleconsulta: '/servicios/teleconsulta',
                   domicilio: '/servicios/consulta-domicilio'
